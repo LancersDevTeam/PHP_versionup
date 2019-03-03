bd92f5b24582141e9382870af42b6f137228f412
Kanazawa Yuki <yakitori009@gmail.com>
2019-03-03 14:35:14 +0900

    Update diff_files.sh

--- PHP5.3toPHP5.6/cake28/Test/CodingChecker/diff_files.sh
+++ PHP5.3toPHP5.6/cake28/Test/CodingChecker/diff_files.sh
@@ -6,7 +6,6 @@ branch=${1}
 
 declare -a tmps=(
     /tmp/diff.log
-    /tmp/tests.log
 )
 
 for file in ${tmps}; do
@@ -19,19 +18,10 @@ echo "branch: ${branch}"
 
 set +e
 git diff -p --name-only --reverse --format="" origin/master...origin/${branch} | sort | uniq > /tmp/diff.log
-#tests=`cat /tmp/diff.log | grep -e '^cake28/Test/Case' | grep 'php$'`
-#if [ "${tests}" ]; then
-#    "${tests}" > /tmp/tests.log
-#fi
 set -e
 
 echo "--------------------------------------------------"
 echo "diff files:"
 echo "`cat /tmp/diff.log`"
-#if [ -f /tmp/tests.log ]; then
-#    echo "--------------------------------------------------"
-#    echo "tests:"
-#    echo "`cat /tmp/tests.log`"
-#fi
 
 exit 0
